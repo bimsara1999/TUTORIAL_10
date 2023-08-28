f21a99881aa68149d749c1ef0558904013173059
bimsara1999 <136779171+bimsara1999@users.noreply.github.com>
2023-08-28 11:58:24 +0545

    Update T10Q2.scala

--- T10Q2.scala
+++ T10Q2.scala
@@ -23,16 +23,4 @@ object T10Q2 {
       }
   
 }
-/*This function is defined to calculate the total count of letter occurrences in a list of words. Inside the function:
 
-The map function is used to transform each word in the input list of words into its length (number of letters). 
-* The _.length is a shorthand for a function that takes a string and returns its length. This results in a list of
-*  integers representing the lengths of the words.
-
-
-The reduce function is then used to calculate the sum of all the word lengths in the list. The _ + _ syntax represents 
-a function that takes two arguments and returns their sum.
-
-The calculated total count of letter occurrences is returned from the function.
-* The countLetterOccurrences function is called with the inputWords list as an argument. The resulting total count 
-* of letter occurrences is stored in the variable totalOccurrences*/
