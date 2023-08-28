82cb59b0498c76b472459a339f947413f079a045
bimsara1999 <136779171+bimsara1999@users.noreply.github.com>
2023-08-28 11:57:43 +0545

    Update T10Q1.scala

--- T10Q1.scala
+++ T10Q1.scala
@@ -27,17 +27,4 @@ println(s"Average Fahrenheit temperature: $averageFahrenheit")
   
   
 }
-/*This is a function named calculateAverage that takes a list of integers (List[Int]) representing Celsius temperatures as its parameter. Inside the function:
 
-It uses the map function to convert each Celsius temperature to Fahrenheit. For each temperature celsius, it applies the formula (celsius * 9/5) + 32 
-to convert it to Fahrenheit. The resulting Fahrenheit temperatures are stored in the fahrenheitTemperatures list.
-
-The reduce function is then used to calculate the sum of the Fahrenheit temperatures. The _ + _ syntax is a shorthand for a function that 
-takes two arguments and returns their sum.
-
-The average Fahrenheit temperature is calculated by dividing the sum of Fahrenheit temperatures by the number of temperatures in the
-input list (temperatures.length).
-
-The calculated average temperature is returned from the function
-* The calculateAverage function is called with the inputTemperatures list as an argument. The resulting average
-*  Fahrenheit temperature is stored in the variable averageFahrenheit.*/
